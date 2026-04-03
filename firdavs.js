b45165dfe457e2681f9205307667973c6fbdd53d
boboyevfirdavs79-rgb <boboyevfirdavs79@gmail.com>
2026-04-03 16:14:53 +0500

    nodir

---
+++ firdavs.js
@@ -0,0 +1,21 @@
+1 - masala
+// function isBalanced(str) {
+//   if (str.length % 2 !== 0) return false;
+
+//   let pairs = {
+//     '(': ')',
+//     '[': ']',
+//     '{': '}'
+//   };
+
+//   for (let i = 0; i < str.length / 2; i++) {
+//     let opening = str[i];
+//     let closing = str[str.length - 1 - i];
+//     if (pairs[opening] !== closing) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isBalanced("({[{([])}]})"));
